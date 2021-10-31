d851de401386dd1e018c78cf9fa3e6ba6976e13c
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-01 00:27:11 +0300

    Define the Sources class and initialize it by passing it's parameters

--- app/models.py
+++ app/models.py
@@ -8,3 +8,8 @@ class NewsArticle:
         self.published_at=published_at
         self.description=description
         self.content=content
+
+class Sources:
+    def __init__(self, source_id, source_name):
+        self.source_id=source_id
+        self.source_name=source_name
