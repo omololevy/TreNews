c2ab95607dfc35d2706a58bbad58fa9ee674caba
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-01 00:25:09 +0300

    Define the NewsArticle class and pass the parameters of this class

---
+++ app/models.py
@@ -0,0 +1,10 @@
+class NewsArticle:
+    def __init__(self, source_name, author, title, url, url_to_image, published_at, description, content):
+        self.source_name=source_name
+        self.author=author
+        self.title=title
+        self.url=url
+        self.url_to_image=url_to_image
+        self.published_at=published_at
+        self.description=description
+        self.content=content
