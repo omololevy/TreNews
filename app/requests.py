d97c84ba5ce1d5e7745b232b872144a138d0fb6f
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-01 08:28:41 +0300

    Create a function that processes and returns the news result

--- app/requests.py
+++ app/requests.py
@@ -4,6 +4,9 @@ from .models import NewsArticle, Sources
 api_key=None
 
 def configure_request(app):
+    '''
+    
+    '''
 
     global api_key
     api_key=app.config['NEWS_API_KEY']
@@ -23,5 +26,33 @@ def get_news(country, category):
             news_results_list = get_news_response['articles']
             news_results = process_news_results(news_results_list)
 
+    return news_results
 
+def process_news_results(news_list):    
+    
+    news_results = []
+    
+    for news_item in news_list:
+        source = news_item.get('source')
+        source_name= source['name']
+        author = news_item.get('author')
+        if author==None:
+            author=source_name
+        elif author==' ' or author=='':
+            author=source_name
+        elif len(author)>40:
+            author=source_name
+        elif author[0:4]=="http":
+            author=source_name
+        title = news_item.get('title')
+        url = news_item.get('url')
+        image_url = news_item.get('urlToImage')        
+        published_at = news_item.get('publishedAt')        
+        published=date_pipe(published_at)
+        description=news_item.get('description')
+        content=news_item.get('content')
+        
+        news_object = NewsArticle(source_name,author,title,url,image_url,published,description,content)
+        news_results.append(news_object)        
+        
     return news_results
