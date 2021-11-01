c77a093a626fe6cd09ac5523544a14afdcb1fc76
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-01 16:19:03 +0300

    Define a function that requests news article from the source

--- app/requests.py
+++ app/requests.py
@@ -75,3 +75,20 @@ def process_news_results(news_list):
         news_results.append(news_object)        
         
     return news_results
+
+def news_from_source(source_id):
+  
+    get_url = 'http://newsapi.org/v2/everything?sources={}&pageSize=30&apiKey={}'.format(source_id, api_key)
+
+    with urllib.request.urlopen(get_url) as url:
+        get_data = url.read()
+        get_response = json.loads(get_data)
+
+        results = None
+
+        if get_response['articles']:
+            results_list = get_response['articles']
+            results = process_news_results(results_list)
+
+
+    return results
