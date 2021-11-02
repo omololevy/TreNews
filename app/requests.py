39a8e337478f2a93c685522fa12c7f2ded80f804
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-02 18:11:40 +0300

    Add the functions news_from_source and get_sources

--- app/requests.py
+++ app/requests.py
@@ -76,6 +76,7 @@ def process_news_results(news_list):
         
     return news_results
 
+
 def news_from_source(source_id):
   
     get_url = 'http://newsapi.org/v2/everything?sources={}&pageSize=30&apiKey={}'.format(source_id, api_key)
@@ -92,3 +93,19 @@ def news_from_source(source_id):
 
 
     return results
+
+def get_sources():
+
+    get_sources_url = 'https://newsapi.org/v2/sources?country=us&category=general&language=en&apiKey={}'.format(api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        sources_results = None
+
+        if get_sources_response['sources']:
+            sources_results_list = get_sources_response['sources']
+            sources_results = process_sources_results(sources_results_list)
+
+    return sources_results
