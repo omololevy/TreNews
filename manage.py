9f9ac326e9d9fe3525f3b025db285bea19616002
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-02 18:19:27 +0300

    Add the test function to perform the unnittest of all the tests

--- manage.py
+++ manage.py
@@ -6,6 +6,13 @@ app= create_app('development')
 manager=Manager(app)
 manager.add_command('server', Server)
 
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    
 
 if __name__=='__main__':
     manager.run()
