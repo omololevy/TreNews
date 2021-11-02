0912aa6a984c7e478c644f1971214a18750387ff
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-03 00:30:09 +0300

    Add doc string to the 404 error function

--- app/main/errors.py
+++ app/main/errors.py
@@ -3,5 +3,11 @@ from . import main
 
 @main.app_errorhandler(404)
 def four_Ow_four(error):
+    '''
+    Function that handles the 404 file not found error
+
+    Args:
+        error
+    '''
     return render_template('fourOwfour.html'),404
     
