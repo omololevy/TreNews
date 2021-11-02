d022497f6a15e01ae8ab1e48fd8fb324c771f953
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-11-03 00:34:07 +0300

    Add the doc string for the created configuration functions

--- config.py
+++ config.py
@@ -1,15 +1,30 @@
 import os
 
 class Config:
+    '''
+    The main configuration class for the application
+    '''
     SECRET_KEY=os.environ.get('SECRET_KEY')
     NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
 
 
 class ProdConfig(Config):
+    '''
+    Class that forms the subclass of the Config class and Debug-effective in the production environment
+
+    Args:
+        Config class
+    '''
     pass
 
 
 class DevConfig(Config):
+    '''
+    Class that is a subclass of the Config class and Debug-effective in the development phase/environment
+
+    Args:
+        Config class
+    '''
     DEBUG=True
 
 
